ccf28de839d529a70b81a95a08115618e57355a7
Gut Jakub <w65539@student.wsiz.edu.pl>
2024-04-17 00:13:31 +0200

    scraping

---
+++ tests/scrape.js
@@ -0,0 +1,23 @@
+const { By, Key, Builder } = require('selenium-webdriver');
+require('chromedriver');
+
+async function scrape() {
+    // create driver
+    let driver = await new Builder().forBrowser('chrome').build();
+
+    try {
+        await driver.get('https://ratings.fide.com/top.phtml?list=men');
+
+        let names = await driver.findElements(By.css('.tur'));
+
+        for (let n of names) {
+            console.log(await n.getText());
+        }
+    } catch (e) {
+        console.log(e);
+    } finally {
+        driver.quit();
+    }
+}
+
+scrape();
